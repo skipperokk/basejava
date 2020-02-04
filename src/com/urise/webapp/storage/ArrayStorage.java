5a9e25d2c98f91fcca698da24688245d004bbe81
skipperokk <Kh9yK$Rf>
2020-02-04 10:24:36 +0300

    fix accesses

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -6,31 +6,31 @@ import com.urise.webapp.model.Resume;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    int countOfResumes = 0;
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int countOfResumes = 0;
 
-    void clear() {
+    public void clear() {
         for (int i = 0; i < countOfResumes; i++) {
             storage[i] = null;
         }
         countOfResumes = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
         storage[countOfResumes++] = r;
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         for (int i = 0; i < countOfResumes; i++) {
-            if (uuid.equals(storage[i].uuid))
+            if (uuid.equals(storage[i].getUuid()))
                 return storage[i];
         }
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         for (int i = 0; i < countOfResumes; i++) {
-            if (storage[i].uuid.equals(uuid)) {
+            if (storage[i].getUuid().equals(uuid)) {
                 for (int j = i; j < countOfResumes - 1; j++) {
                     storage[j] = storage[j + 1];
                 }
@@ -42,7 +42,7 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         Resume[] resumes = new Resume[countOfResumes];
         for (int i = 0; i < countOfResumes; i++) {
             resumes[i] = storage[i];
@@ -50,7 +50,7 @@ public class ArrayStorage {
         return resumes;
     }
 
-    int size() {
+    public int size() {
         return countOfResumes;
     }
 }
