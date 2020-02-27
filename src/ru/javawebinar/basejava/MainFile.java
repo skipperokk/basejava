aea6ecd35b4863719ad06d8c0e4c8ae24b6b12ef
skipperokk <Kh9yK$Rf>
2020-02-27 19:45:02 +0300

    Lesson_8 HW_8 part_2 (recursion)

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -3,6 +3,9 @@ package ru.javawebinar.basejava;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class MainFile {
     public static void main(String[] args) {
@@ -13,8 +16,7 @@ public class MainFile {
         } catch (IOException e) {
             throw new RuntimeException("Error", e);
         }
-        File dir = new File("./src/ru/javawebinar/basejava");
-        System.out.println(dir.isDirectory());
+        File dir = new File("C:\\Users\\Admin\\Desktop\\basejava");
         String[] list = dir.list();
         if (list != null) {
             for (String name : list) {
@@ -22,10 +24,47 @@ public class MainFile {
             }
         }
 
-        try (FileInputStream fis = new FileInputStream(filePath)) {
+        try (
+                FileInputStream fis = new FileInputStream(filePath)) {
             System.out.println(fis.read());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        //HomeWork Les.8 part 2
+
+        final String path = "./";
+        List<String> res = new ArrayList<>();
+
+        try {
+            directory(path, res);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        for (String s : res)
+            System.out.println(s);
+    }
+
+    private static void directory(String path, List<String> res) {
+
+        File dir = new File(path);
+        File[] list;
+        try {
+            //Не проходит по скрытым каталогам
+            if (!dir.isHidden()) {
+                list = dir.listFiles();
+                for (File file : Objects.requireNonNull(list)) {
+                    if (file.isFile()) {
+                        res.add(file.getName());
+                    } else {
+                        directory(file.getCanonicalPath(), res);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
+
