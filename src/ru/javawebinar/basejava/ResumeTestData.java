6773e575cbb7e43a353a6da081fad33c9a772dca
skipperokk <Kh9yK$Rf>
2020-02-27 16:37:44 +0300

    Lesson_8 HW_8 part_1

--- src/ru/javawebinar/basejava/ResumeTestData.java
+++ src/ru/javawebinar/basejava/ResumeTestData.java
@@ -41,27 +41,57 @@ public class ResumeTestData {
         )));
 
         resume.addSection(SectionType.EXPERIENCE, new OrganizationSection(Arrays.asList(
-                new Organization(YearMonth.of(2013, 10), YearMonth.now(), "Автор проекта", "Создание, организация и проведение Java онлайн проектов и стажировок", "Java Online Projects", "http://javaops.ru/"),
-                new Organization(YearMonth.of(2014, 10), YearMonth.of(2016, 1), "Старший разработчик (backend)", "Проектирование и разработка онлайн платформы управления проектами Wrike (Java 8 API, Maven, Spring, MyBatis, Guava, Vaadin, PostgreSQL, Redis). Двухфакторная аутентификация, авторизация по OAuth1, OAuth2, JWT SSO.", "Wrike", "https://www.wrike.com/"),
-                new Organization(YearMonth.of(2012, 4), YearMonth.of(2014, 10), "Java архитектор", "Организация процесса разработки системы ERP для разных окружений: релизная политика, версионирование, ведение CI (Jenkins), миграция базы (кастомизация Flyway), конфигурирование системы (pgBoucer, Nginx), AAA via SSO. Архитектура БД и серверной части системы. Разработка интергационных сервисов: CMIS, BPMN2, 1C (WebServices), сервисов общего назначения (почта, экспорт в pdf, doc, html). Интеграция Alfresco JLAN для online редактирование из браузера документов MS Office. Maven + plugin development, Ant, Apache Commons, Spring security, Spring MVC, Tomcat,WSO2, xcmis, OpenCmis, Bonita, Python scripting, Unix shell remote scripting via ssh tunnels, PL/Python", "RIT Center", ""),
-                new Organization(YearMonth.of(2010, 12), YearMonth.of(2012, 4), "Ведущий программист", "Участие в проекте Deutsche Bank CRM (WebLogic, Hibernate, Spring, Spring MVC, SmartGWT, GWT, Jasper, Oracle). Реализация клиентской и серверной части CRM. Реализация RIA-приложения для администрирования, мониторинга и анализа результатов в области алгоритмического трейдинга. JPA, Spring, Spring-MVC, GWT, ExtGWT (GXT), Highstock, Commet, HTML5.", "Luxoft (Deutsche Bank)", "http://www.luxoft.ru/"),
-                new Organization(YearMonth.of(2008, 6), YearMonth.of(2010, 12), "Ведущий специалист", "Дизайн и имплементация Java EE фреймворка для отдела \"Платежные Системы\" (GlassFish v2.1, v3, OC4J, EJB3, JAX-WS RI 2.1, Servlet 2.4, JSP, JMX, JMS, Maven2). Реализация администрирования, статистики и мониторинга фреймворка. Разработка online JMX клиента (Python/ Jython, Django, ExtJS)", "Yota", "https://www.yota.ru/"),
-                new Organization(YearMonth.of(2007, 3), YearMonth.of(2008, 6), "Разработчик ПО", "Реализация клиентской (Eclipse RCP) и серверной (JBoss 4.2, Hibernate 3.0, Tomcat, JMS) частей кластерного J2EE приложения (OLAP, Data mining).", "Enkata", "http://enkata.com/"),
-                new Organization(YearMonth.of(2005, 1), YearMonth.of(2007, 2), "Разработчик ПО", "Разработка информационной модели, проектирование интерфейсов, реализация и отладка ПО на мобильной IN платформе Siemens @vantage (Java, Unix).", "Siemens AG", "https://www.siemens.com/ru/ru/home.html"),
-                new Organization(YearMonth.of(1997, 9), YearMonth.of(2005, 1), "Инженер по аппаратному и программному тестированию", "Тестирование, отладка, внедрение ПО цифровой телефонной станции Alcatel 1000 S12 (CHILL, ASM).", "Alcatel", "http://www.alcatel.ru/")
+                new Organization("Java Online Projects", "http://javaops.ru/",
+                        Arrays.asList(
+                                new ListDescriptions(YearMonth.of(2013, 10), YearMonth.now(), "Автор проекта", "Создание, организация и проведение Java онлайн проектов и стажировок"))),
+                new Organization("Wrike", "https://www.wrike.com/",
+                        Arrays.asList(
+                                new ListDescriptions(YearMonth.of(2014, 10), YearMonth.of(2016, 01), "Старший разработчик (backend)", "Проектирование и разработка онлайн платформы управления проектами Wrike (Java 8 API, Maven, Spring, MyBatis, Guava, Vaadin, PostgreSQL, Redis). Двухфакторная аутентификация, авторизация по OAuth1, OAuth2, JWT SSO."))),
+                new Organization("RIT Center", "",
+                        Arrays.asList(
+                                new ListDescriptions(YearMonth.of(2012, 4), YearMonth.of(2014, 10), "Java архитектор", "Организация процесса разработки системы ERP для разных окружений: релизная политика, версионирование, ведение CI (Jenkins), миграция базы (кастомизация Flyway), конфигурирование системы (pgBoucer, Nginx), AAA via SSO. Архитектура БД и серверной части системы. Разработка интергационных сервисов: CMIS, BPMN2, 1C (WebServices), сервисов общего назначения (почта, экспорт в pdf, doc, html). Интеграция Alfresco JLAN для online редактирование из браузера документов MS Office. Maven + plugin development, Ant, Apache Commons, Spring security, Spring MVC, Tomcat,WSO2, xcmis, OpenCmis, Bonita, Python scripting, Unix shell remote scripting via ssh tunnels, PL/Python"))),
+                new Organization("Luxoft (Deutsche Bank)", "http://www.luxoft.ru/",
+                        Arrays.asList(
+                                new ListDescriptions(YearMonth.of(2010, 12), YearMonth.of(2012, 4), "Ведущий программист", "Участие в проекте Deutsche Bank CRM (WebLogic, Hibernate, Spring, Spring MVC, SmartGWT, GWT, Jasper, Oracle). Реализация клиентской и серверной части CRM. Реализация RIA-приложения для администрирования, мониторинга и анализа результатов в области алгоритмического трейдинга. JPA, Spring, Spring-MVC, GWT, ExtGWT (GXT), Highstock, Commet, HTML5."))),
+                new Organization("Yota", "https://www.yota.ru/",
+                        Arrays.asList(
+                                new ListDescriptions(YearMonth.of(2008, 6), YearMonth.of(2010, 12), "Ведущий специалист", "Дизайн и имплементация Java EE фреймворка для отдела \"Платежные Системы\" (GlassFish v2.1, v3, OC4J, EJB3, JAX-WS RI 2.1, Servlet 2.4, JSP, JMX, JMS, Maven2). Реализация администрирования, статистики и мониторинга фреймворка. Разработка online JMX клиента (Python/ Jython, Django, ExtJS)"))),
+                new Organization("Enkata", "http://enkata.com/",
+                        Arrays.asList(
+                                new ListDescriptions(YearMonth.of(2007, 3), YearMonth.of(2008, 6), "Разработчик ПО", "Реализация клиентской (Eclipse RCP) и серверной (JBoss 4.2, Hibernate 3.0, Tomcat, JMS) частей кластерного J2EE приложения (OLAP, Data mining)"))),
+                new Organization("Siemens AG", "https://www.siemens.com/ru/ru/home.html",
+                        Arrays.asList(
+                                new ListDescriptions(YearMonth.of(2005, 1), YearMonth.of(2007, 2), "Разработчик ПО", "Разработка информационной модели, проектирование интерфейсов, реализация и отладка ПО на мобильной IN платформе Siemens @vantage (Java, Unix)."))),
+                new Organization("Alcatel", "http://www.alcatel.ru/",
+                        Arrays.asList(
+                                new ListDescriptions(YearMonth.of(1997, 9), YearMonth.of(2005, 1), "Инженер по аппаратному и программному тестированию", "Тестирование, отладка, внедрение ПО цифровой телефонной станции Alcatel 1000 S12 (CHILL, ASM).")))
         )));
 
         resume.addSection(SectionType.EDUCATION, new OrganizationSection(Arrays.asList(
-                new Organization(YearMonth.of(2013, 3), YearMonth.of(2013, 5), "Functional Programming Principles in Scala by Martin Odersky", "", "Coursera", "https://www.coursera.org/course/progfun"),
-                new Organization(YearMonth.of(2011, 3), YearMonth.of(2011, 4), "Курс Объектно-ориентированный анализ ИС. Концептуальное моделирование на UML.", "", "Luxoft", "http://www.luxoft-training.ru/training/catalog/course.html?ID=22366"),
-                new Organization(YearMonth.of(2005, 1), YearMonth.of(2005, 4), "3 месяца обучения мобильным IN сетям (Берлин)", "", "Siemens AG", "http://www.siemens.ru/"),
-                new Organization(YearMonth.of(1997, 9), YearMonth.of(1998, 3), "6 месяцев обучения цифровым телефонным сетям (Москва)", "", "Alcatel", "http://www.alcatel.ru/"),
-                new Organization(YearMonth.of(1993, 9), YearMonth.of(1996, 7), "Аспирантура (программист С, С++)", "", "Санкт-Петербургский национальный исследовательский университет информационных технологий, механики и оптики", "http://www.ifmo.ru/"),
-                new Organization(YearMonth.of(1987, 9), YearMonth.of(1993, 7), "Инженер (программист Fortran, C)", "", "", ""),
-                new Organization(YearMonth.of(1984, 9), YearMonth.of(1987, 6), "Закончил с отличием", "", "Заочная физико-техническая школа при МФТИ", "http://www.school.mipt.ru/")
+                new Organization("Coursera", "https://www.coursera.org/course/progfun",
+                        Arrays.asList(
+                                new ListDescriptions(YearMonth.of(2013, 3), YearMonth.of(2013, 5), "Functional Programming Principles in Scala by Martin Odersky", ""))),
+                new Organization("Luxoft", "http://www.luxoft-training.ru/training/catalog/course.html?ID=22366",
+                        Arrays.asList(
+                                new ListDescriptions(YearMonth.of(2011, 3), YearMonth.of(2011, 4), "Курс Объектно-ориентированный анализ ИС. Концептуальное моделирование на UML.", ""))),
+                new Organization("Siemens AG", "http://www.siemens.ru/",
+                        Arrays.asList(
+                                new ListDescriptions(YearMonth.of(2005, 1), YearMonth.of(2005, 4), "3 месяца обучения мобильным IN сетям (Берлин)", ""))),
+                new Organization("Alcatel", "http://www.alcatel.ru/",
+                        Arrays.asList(
+                                new ListDescriptions(YearMonth.of(1997, 9), YearMonth.of(1998, 3), "6 месяцев обучения цифровым телефонным сетям (Москва)", ""))),
+                new Organization("Санкт-Петербургский национальный исследовательский университет информационных технологий, механики и оптики", "http://www.ifmo.ru/",
+                        Arrays.asList(
+                                new ListDescriptions(YearMonth.of(1993, 9), YearMonth.of(1996, 7), "Аспирантура (программист С, С++)", ""),
+                                new ListDescriptions(YearMonth.of(1987, 9), YearMonth.of(1993, 7), "Инженер (программист Fortran, C)", ""))),
+                new Organization("Заочная физико-техническая школа при МФТИ", "http://www.school.mipt.ru/",
+                        Arrays.asList(
+                                new ListDescriptions(YearMonth.of(1984, 9), YearMonth.of(1987, 6), "Закончил с отличием", "")))
         )));
 
-        resume.addContact(ContactType.PHONE, "+79969745519");
+        resume.addContact(ContactType.PHONE, "+7-(495)-999-99-99");
+        resume.addContact(ContactType.MOBILE, "+79969745519");
+        resume.addContact(ContactType.HOME_PHONE, "+7-(495)-999-99-11");
         resume.addContact(ContactType.SKYPE, new Link("grigory.kislin", "skype:grigory.kislin").getName());
         resume.addContact(ContactType.MAIL, new Link("gkislin@yandex.ru", "mailto:gkislin@yandex.ru").getName());
         resume.addContact(ContactType.LINKEDIN, new Link("Профиль LinkedIn", "https://www.linkedin.com/in/gkislin").getName());
@@ -78,5 +108,6 @@ public class ResumeTestData {
         for (SectionType sectionType : SectionType.values()) {
             System.out.println(resume.getSection(sectionType));
         }
+
     }
 }
--- src/ru/javawebinar/basejava/model/Organization.java
+++ src/ru/javawebinar/basejava/model/Organization.java
@@ -1,94 +1,28 @@
 package ru.javawebinar.basejava.model;
 
-import java.time.YearMonth;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class Organization {
-
     private final Link homePage;
-    private List<OrganizationDescribeBlock> odb;
+    private List<ListDescriptions> descriptionList = new ArrayList<>();
 
-    public Organization(String name, String url, List<OrganizationDescribeBlock> odb){
-        this(new Link(name, url), odb);
+    public Organization(String name, String url, List<ListDescriptions> descriptionList) {
+        this.homePage = new Link(name, url);
+        this.descriptionList = descriptionList;
     }
 
-    public Organization(Link homePage, List<OrganizationDescribeBlock> odb) {
-        this.homePage = homePage;
-        this.odb = odb;
+    public Link getHomePage() {
+        return homePage;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Organization that = (Organization) o;
-
-        if (!Objects.equals(homePage, that.homePage)) return false;
-        return Objects.equals(odb, that.odb);
+    public List<ListDescriptions> getDescriptionList() {
+        return descriptionList;
     }
 
     @Override
-    public int hashCode() {
-        int result = homePage != null ? homePage.hashCode() : 0;
-        result = 31 * result + (odb != null ? odb.hashCode() : 0);
-        return result;
-    }
-
-    private static class OrganizationDescribeBlock {
-        private final YearMonth startDate;
-        private final YearMonth endDate;
-        private final String title;
-        private final String description;
-
-        public OrganizationDescribeBlock(YearMonth startDate, YearMonth endDate, String title, String description) {
-            Objects.requireNonNull(startDate, "Start date must not be null");
-            Objects.requireNonNull(endDate, "End date must not be null");
-            Objects.requireNonNull(title, "Title must not be null");
-            this.startDate = startDate;
-            this.endDate = endDate;
-            this.title = title;
-            this.description = description;
-
-        }
-
-        public YearMonth getStartDate() {
-            return startDate;
-        }
-
-        public YearMonth getEndDate() {
-            return endDate;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            OrganizationDescribeBlock that = (OrganizationDescribeBlock) o;
-
-            if (!startDate.equals(that.startDate)) return false;
-            if (!endDate.equals(that.endDate)) return false;
-            if (!title.equals(that.title)) return false;
-            return Objects.equals(description, that.description);
-        }
-
-        @Override
-        public int hashCode() {
-            int result = startDate.hashCode();
-            result = 31 * result + endDate.hashCode();
-            result = 31 * result + title.hashCode();
-            result = 31 * result + (description != null ? description.hashCode() : 0);
-            return result;
-        }
+    public String toString() {
+        return "Organization{" +
+                "homePage=" + homePage + "Description=" + descriptionList.toString() + "}";
     }
 }
