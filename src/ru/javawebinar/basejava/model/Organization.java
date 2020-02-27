15cf7fee8fb0f734f4e9980dc74328cc97144226
skipperokk <Kh9yK$Rf>
2020-02-27 11:17:28 +0300

    Lesson_8 HW_8 fix Organization

--- src/ru/javawebinar/basejava/model/Organization.java
+++ src/ru/javawebinar/basejava/model/Organization.java
@@ -1,25 +1,21 @@
 package ru.javawebinar.basejava.model;
 
 import java.time.YearMonth;
+import java.util.List;
 import java.util.Objects;
 
 public class Organization {
-    private final YearMonth startDate;
-    private final YearMonth endDate;
-    private final String title;
-    private final String description;
 
     private final Link homePage;
+    private List<OrganizationDescribeBlock> odb;
 
-    public Organization(YearMonth startDate, YearMonth endDate, String title, String description, String name, String url) {
-        Objects.requireNonNull(startDate, "Start date must not be null");
-        Objects.requireNonNull(endDate, "End date must not be null");
-        Objects.requireNonNull(title, "Title must not be null");
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.title = title;
-        this.description = description;
-        this.homePage = new Link(name, url);
+    public Organization(String name, String url, List<OrganizationDescribeBlock> odb){
+        this(new Link(name, url), odb);
+    }
+
+    public Organization(Link homePage, List<OrganizationDescribeBlock> odb) {
+        this.homePage = homePage;
+        this.odb = odb;
     }
 
     @Override
@@ -29,26 +25,70 @@ public class Organization {
 
         Organization that = (Organization) o;
 
-        if (!startDate.equals(that.startDate)) return false;
-        if (!endDate.equals(that.endDate)) return false;
-        if (!title.equals(that.title)) return false;
-        if (!Objects.equals(description, that.description)) return false;
-        return homePage.equals(that.homePage);
+        if (!Objects.equals(homePage, that.homePage)) return false;
+        return Objects.equals(odb, that.odb);
     }
 
     @Override
     public int hashCode() {
-        int result = startDate.hashCode();
-        result = 31 * result + endDate.hashCode();
-        result = 31 * result + title.hashCode();
-        result = 31 * result + (description != null ? description.hashCode() : 0);
-        result = 31 * result + homePage.hashCode();
+        int result = homePage != null ? homePage.hashCode() : 0;
+        result = 31 * result + (odb != null ? odb.hashCode() : 0);
         return result;
     }
 
-    @Override
-    public String toString() {
-        return startDate + " - "
-                + endDate + ", " + title + ", " + description + ", " + homePage;
+    private static class OrganizationDescribeBlock {
+        private final YearMonth startDate;
+        private final YearMonth endDate;
+        private final String title;
+        private final String description;
+
+        public OrganizationDescribeBlock(YearMonth startDate, YearMonth endDate, String title, String description) {
+            Objects.requireNonNull(startDate, "Start date must not be null");
+            Objects.requireNonNull(endDate, "End date must not be null");
+            Objects.requireNonNull(title, "Title must not be null");
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.title = title;
+            this.description = description;
+
+        }
+
+        public YearMonth getStartDate() {
+            return startDate;
+        }
+
+        public YearMonth getEndDate() {
+            return endDate;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            OrganizationDescribeBlock that = (OrganizationDescribeBlock) o;
+
+            if (!startDate.equals(that.startDate)) return false;
+            if (!endDate.equals(that.endDate)) return false;
+            if (!title.equals(that.title)) return false;
+            return Objects.equals(description, that.description);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = startDate.hashCode();
+            result = 31 * result + endDate.hashCode();
+            result = 31 * result + title.hashCode();
+            result = 31 * result + (description != null ? description.hashCode() : 0);
+            return result;
+        }
     }
 }
