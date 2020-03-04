2d9a8ed55e69fcc8aa8e7c7b88613675f83362ac
skipperokk <Kh9yK$Rf>
2020-03-04 13:58:13 +0300

    Lesson_9 HW_9 part_3 fix add FileStorageTest

---
+++ test/ru/javawebinar/basejava/storage/FileStorageTest.java
@@ -0,0 +1,8 @@
+package ru.javawebinar.basejava.storage;
+
+public class FileStorageTest extends AbstractStorageTest {
+
+    public FileStorageTest() {
+        super(new FileStorage(STORAGE_DIR, new ObjectStreamStorage()));
+    }
+}
