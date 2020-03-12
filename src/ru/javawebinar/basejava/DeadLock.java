d9db4d71370784dbe8631f8cd7e3f39c2f1fb602
skipperokk <Kh9yK$Rf>
2020-03-12 12:14:18 +0300

    Lesson_11 HW11 fix

--- src/ru/javawebinar/basejava/DeadLock.java
+++ src/ru/javawebinar/basejava/DeadLock.java
@@ -2,37 +2,39 @@ package ru.javawebinar.basejava;
 
 public class DeadLock {
     public static void main(String[] args) {
-        Object object1 = new Object();
-        Object object2 = new Object();
+        Object object1 = new Object("Object_1");
+        Object object2 = new Object("Object_2");
+        makeThreadWithObject(object1, object2);
+        makeThreadWithObject(object2, object1);
+    }
 
+    private static void makeThreadWithObject(Object object1, Object object2) {
         new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + ": захватил монитор object1");
+            System.out.println(Thread.currentThread().getName() + ": захватил монитор " + object1);
             synchronized (object1) {
-                System.out.println(Thread.currentThread().getName() + ": ожидает захвата монитора object2");
+                System.out.println(Thread.currentThread().getName() + ": ожидает захвата монитора" + object2);
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
                 synchronized (object2) {
-                    System.out.println(Thread.currentThread().getName() + ": захватил монитор object2");
+                    System.out.println(Thread.currentThread().getName() + ": захватил монитор" + object2);
                 }
             }
         }).start();
+    }
+}
 
-        new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + ": захватил монитор object2");
-            synchronized (object2) {
-                System.out.println(Thread.currentThread().getName() + ": ожидает захвата монитора object2");
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (object1) {
-                    System.out.println(Thread.currentThread().getName() + ": захватил монитор object1");
-                }
-            }
-        }).start();
+class Object {
+    private final String name;
+
+    public Object(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
